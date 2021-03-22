963616ba3b0ede316572a4716bd38bd02157884e
Daniel Verkamp <dverkamp@chromium.org>
2021-03-22 15:07:55 -0700

    Import crosvm FDT writer
    Signed-off-by: Daniel Verkamp <dverkamp@chromium.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
 #![deny(missing_docs)]
-//! Dummy crate needs high-level documentation.
-/// Dummy public function needs documentation.
-pub fn it_works() {
-    assert!(true);
-}
+
+//! This crate provides the ability to manipulate Flattened Devicetree blobs.
+
+mod writer;
+
+pub use writer::FdtWriter;
+pub use writer::Result as FdtWriterResult;
+
+/// Magic number used in the FDT header.
+const FDT_MAGIC: u32 = 0xd00dfeed;
+
+const FDT_BEGIN_NODE: u32 = 0x00000001;
+const FDT_END_NODE: u32 = 0x00000002;
+const FDT_PROP: u32 = 0x00000003;
+const FDT_END: u32 = 0x00000009;
