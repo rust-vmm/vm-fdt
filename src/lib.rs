4ce11452b1115ab75b925795f4b824c3e6697baa
Andreea Florescu <fandree@amazon.com>
2021-07-07 15:31:58 +0300

    add usage example in src/lib.rs
    This example is the same as the one in the README.md. The reason of
    duplicating it in src/lib.rs as well is that with this one we can check
    that the example is actually correctly running as it will be run as part
    of cargo test.
    
    Removed the example from src/writer.rs as it is obsolete.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,61 @@
 #![deny(missing_docs)]
 
 //! This crate provides the ability to manipulate Flattened Devicetree blobs.
+//!
+//! # Example
+//!
+//! In the following example we create an FDT blob with a root node, and 2
+//! children nodes. More details about this example are available in the readme.
+//!
+//! ```rust
+//! use vm_fdt::{Error, FdtWriter};
+//!
+//! fn create_fdt() -> Result<Vec<u8>, Error> {
+//!     let mut fdt = FdtWriter::new()?;
+//!
+//!     let root_node = fdt.begin_node("root")?;
+//!     fdt.property_string("compatible", "linux,dummy-virt")?;
+//!     fdt.property_u32("#address-cells", 0x2)?;
+//!     fdt.property_u32("#size-cells", 0x2)?;
+//!
+//!     let chosen_node = fdt.begin_node("chosen")?;
+//!     fdt.property_u32("linux,pci-probe-only", 1)?;
+//!     fdt.property_string("bootargs", "panic=-1 console=hvc0")?;
+//!     fdt.end_node(chosen_node)?;
+//!
+//!     let memory_node = fdt.begin_node("memory")?;
+//!     fdt.property_string("device_type", "memory")?;
+//!     fdt.end_node(memory_node)?;
+//!
+//!     fdt.end_node(root_node)?;
+//!
+//!     fdt.finish()
+//! }
+//!
+//! # let dtb = create_fdt().unwrap();
+//! ```
+//!
+//! By default the FDT does not have any memory reservations. If the user
+//! needs to add memory reservations as well, then a different constructor
+//! can be used as follows:
+//!
+//! ```rust
+//! use vm_fdt::{Error, FdtReserveEntry, FdtWriter};
+//!
+//! fn create_fdt() -> Result<Vec<u8>, Error> {
+//!     let mut fdt = FdtWriter::new_with_mem_reserv(&[
+//!         FdtReserveEntry::new(0x12345678AABBCCDD, 0x1234).unwrap(),
+//!         FdtReserveEntry::new(0x1020304050607080, 0x5678).unwrap(),
+//!     ])?;
+//!     let root_node = fdt.begin_node("root")?;
+//!     // ... add other nodes & properties
+//!     fdt.end_node(root_node)?;
+//!
+//!     fdt.finish()
+//! }
+//!
+//! # let dtb = create_fdt().unwrap();
+//! ```
 
 mod writer;
 
